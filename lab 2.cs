46b2f0a51cca22965fa930f3ebb4958f1e1d1f20
mjhatley <mjhatley21@email.com>
2018-01-23 14:39:42 -0500

    lab 2

---
+++ lab 2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace lab_2
+{
+    class MainClass
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! enter any integer between 1-100");
+            int input = Convert.ToInt32(Console.ReadLine());
+                               
+            Console.WriteLine("Output:");
+
+            if(input%2==0)
+            {
+                Console.WriteLine("Even");
+            }
+
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+
+            Console.WriteLine("Enter a number between 1 and 100");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Output:");
+
+
+            if(input2%2==0 && input2>=2 && input2<=25)
+            {
+                Console.WriteLine("Even and less than 25");
+            }
+
+            else if(input2%2==0 && input2>25 && input2 <61)
+            {
+                Console.WriteLine("even");
+            }
+
+            else if(input2%2==0 && input2>60)
+            {
+                
+                Console.WriteLine("even");
+            }
+
+            else
+            {
+                Console.WriteLine("odd");
+            }
+
+          
+            Console.WriteLine("Enter a number between 1 and 100");
+            int input3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Output:");
+
+            if (input3 % 2 == 0 && input3 >= 2 && input3 <= 25)
+            {
+                Console.WriteLine("Even and less than 25");
+            }
+
+            else if (input3 % 2 == 0 && input3 > 25 && input3 < 61)
+            {
+                Console.WriteLine("even");
+            }
+
+            else if (input3 % 2 == 0 && input3 > 60)
+            {
+
+                Console.WriteLine(input3 + " and even");
+            }
+
+            else if (input3 % 2 ==1 && input3 >60)
+            {
+                Console.WriteLine(input3 + " and Odd");
+            }
+
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
