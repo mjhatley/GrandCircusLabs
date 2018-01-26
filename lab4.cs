663682163e02ed4fb4d581894d3e85b092aa1962
mjhatley <mjhatley21@email.com>
2018-01-25 20:10:11 -0500

    labs

---
+++ lab4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab4_
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string input = "";
+            do
+            {
+
+                Console.WriteLine("Hello! enter an integer between 1 - 100");
+                long x = long.Parse(Console.ReadLine());
+                long y = 1;
+                for (int i = 1; i <= x; i++)
+                {
+                    y = y * i;
+
+                }
+                Console.WriteLine(y);
+
+                Console.WriteLine("Continue? y/n");
+                input = Console.ReadLine();
+
+            } while (input.ToLower() == "y");
+                
+        }
+    }
+}
---
+++ lab5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lab5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string input = "";
+
+            Console.WriteLine("Would you like to roll the dice? y / n");
+            input = Console.ReadLine();
+
+
+
+            while (input.ToLower() == "y")
+            {
+                
+            Console.WriteLine("Enter the number of sides you would like on your two dice!");
+            int sides = int.Parse(Console.ReadLine());
+
+
+                Random random = new Random();
+
+                int x = random.Next(1, sides);
+
+                int y = random.Next(1, sides);
+
+                Console.WriteLine("Roll 1: " + x);
+
+                Console.WriteLine("Roll 2: " + y);
+
+                Console.WriteLine("Would you like to roll again? y/n");
+                input = Console.ReadLine();
+            }
+
+
+                //answer == "y"
+
+
+
+        }
+    }
+}
