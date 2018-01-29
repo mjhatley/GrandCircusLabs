aa8c5565e5295721c978231ad91d85b880919abe
mjhatley <mjhatley21@email.com>
2018-01-29 08:10:05 -0500

    lab 6

---
+++ lab6.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace lab6
+{
+    class MainClass
+    {
+            
+        public static void Main(string[] args)
+        {
+            string response;
+            do
+            {
+                Console.WriteLine("Welcome to the Pig Latin Translator!");
+
+                Console.WriteLine("Enter a word to be translated!");
+                string input = Console.ReadLine();
+
+                input = input.ToLower();
+
+
+
+                char firstletter = input[0];
+
+                if (isVowel(firstletter))
+                {
+                    input = input + "way";
+                }
+
+                else
+                {
+
+                    while (!isVowel(firstletter))
+                    {
+                        input = input.Remove(0, 1);
+                        input = input + firstletter;
+                        firstletter = input[0];
+                    }
+
+                    input = input + "ay";
+
+                }
+
+
+                Console.WriteLine(input);
+
+                Console.WriteLine("would you like to translate another word?");
+                response = Console.ReadLine();
+            }while(response.ToLower() == "y");
+
+
+        }
+        public static void PrintCharArray(char[] words)
+        {
+            foreach (char letter in words)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+        public static bool isVowel(char c)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            foreach (char vowel in vowels)
+            {
+                if (c == vowel)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+    }
+
+
+
+
+
+
+} 
